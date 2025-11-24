5c53fa596a38bef41076e16d69c4b36b3b534537
Mkaluser <nikitifabandi@gamil.com>
2025-11-24 18:34:04 -0300

    foi

---
+++ Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] letras = new char[10];
+            int i;
+            
+
+            Console.WriteLine("Coloque 10 letras, e nós juntaremos em uma palavra");
+            for(i = 0; i< 10; i++)
+            {
+                letras[i] = char.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(letras);
+            for (i = 0; i < 10; i++)
+            {
+                Console.WriteLine(letras[i]);
+            }
+        }
+    }
+}
---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args) 
+        {
+
+            int i;
+            int a;
+
+            Console.WriteLine("Quantas letras você quer na sua palavra?");
+            a = int.Parse(Console.ReadLine());
+            char[] letras = new char[a];
+
+            Console.WriteLine("Coloque " + a + " letras, e nós juntaremos em uma palavra");
+            for (i = 0; i < a; i++)
+            {
+                letras[i] = char.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("A palavra completa é: ");
+            Console.WriteLine(letras);
+            for (i = 0; i < a; i++)
+            {
+                int j = i + 1;
+                Console.WriteLine("a "+j+"° letra é:"+letras[i]);
+
+            }
+        }
+    }
+}
